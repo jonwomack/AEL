2e845a1e430531f15a6ef41b4be03a4b831a420b
jonathanglennwomack <jonathanglennwomack@gmail.com>
2019-03-23 18:51:49 -0400

    Android Heading

---
+++ test.php
@@ -0,0 +1,19 @@
+<html>
+  <head>
+    <title>A quick test</title>
+  </head>
+  <body>
+  <form action="/welcome.php" method="get">
+  Username:<br>
+  <input type="text" name="username" value="Mickey">
+  <br>
+  Password:<br>
+  <input type="text" name="password" value="Mouse">
+  <br><br>
+  <input type="submit" value="Submit">
+  </form>
+  </body>
+
+
+
+</html>
---
+++ welcome.php
@@ -0,0 +1,49 @@
+<html>
+<body>
+
+ <?php
+   class MyDB extends SQLite3 {
+      function __construct() {
+         $this->open('test.db');
+      }
+   }
+   $db = new MyDB();
+   if(!$db) {
+      echo $db->lastErrorMsg();
+   } else {
+  }
+$query = $db->query('SELECT user_name, password FROM users LIMIT 25'); // buffered result set
+
+ $result = resultSetToArray($query);
+ $loggedIn = false;
+foreach ($result as $entry) {
+ if( $entry['user_name'] == $_GET["username"] && $entry['password'] == $_GET["password"]) {
+ header('Location: index.html');
+ $loggedIn = true;
+ exit;
+ }
+ }
+ if ($loggedIn !== true) {
+     header('Location: test.php');r
+     exit;
+ }
+
+
+
+
+
+  function resultSetToArray($queryResultSet) {
+    $multiArray = array();
+    $count = 0;
+    while($row = $queryResultSet->fetchArray(SQLITE3_ASSOC)){
+        foreach($row as $i=>$value) {
+            $multiArray[$count][$i] = $value;
+        }
+        $count++;
+    }
+    return $multiArray;
+}
+  ?>
+
+</body>
+</html>
